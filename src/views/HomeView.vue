fdfabb92e9b9a154e2a0bb43879b5cfb29b94e81
Keith Gathimba <Keith.Gathimba@Student.HTW-Berlin.de>
2025-11-21 01:19:19 +0100

    Updated HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,22 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import{ type Book } from '../types/Book';
+import { ref, onMounted } from 'vue';
+import { type Book } from '../types/Book';
 import BookList from "../components/BookList.vue";
 
-const myBooks = ref<Book[]>([
-  { id: 1, title: 'Der Name der Rose', author: 'Umberto Eco' },
-  { id: 2, title: 'Die Tribute von Panem', author: 'Suzanne Collins' },
-  { id: 3, title: 'Siddhartha', author: 'Hermann Hesse' },
-]);
+const myBooks = ref<Book[]>([]);
+
+onMounted(() => {
+  fetch('https://webtech-backend-g4ak.onrender.com/api/v1/books')
+    .then(response => response.json())
+    .then(data => {
+      myBooks.value = data;
+    })
+    .catch(error => console.error('Fehler aufgetreten:', error));
+});
 </script>
 
 <template>
-  <main>np
+  <main>
     <BookList :books="myBooks" />
   </main>
 </template>
